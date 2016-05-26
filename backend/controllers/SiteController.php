5e28105d2ee4b2280f37b761569205575102aae6
Home <tuananh3988@gmail.com>
2016-05-26 23:22:46 +0700

    add template gentallela

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -22,11 +22,11 @@ class SiteController extends Controller
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'actions' => ['login', 'error'],
+                        'actions' => ['login', 'error', 'index'],
                         'allow' => true,
                     ],
                     [
-                        'actions' => ['logout', 'index'],
+                        'actions' => ['logout'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
@@ -55,6 +55,7 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
+        $this->layout = 'gentellela';
         return $this->render('index');
     }
 
