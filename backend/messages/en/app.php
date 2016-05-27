c3c82710d3d85782871628dbf514d6d6ea8345a3
votinh12345 <nguyenvanhienbk2006@gmail.com>
2016-05-27 10:20:03 +0700

    add login

---
+++ backend/messages/en/app.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return[
+    'required' => 'Please enter your {attribute}',
+    'validation use' => 'Invalid id or password.'
+];
